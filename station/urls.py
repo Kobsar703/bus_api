dac373859140b897142444e83ccd05dec6f466e9
Oleksandr Kobzar <alexkobsar1001@gmail.com>
2022-12-28 12:44:14 -0400

    Change api to class based view generics and mixins

--- station/urls.py
+++ station/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from station.views import bus_list
+from station.views import bus_list, bus_detail, BusList, BusDetail, BusListGen, BusDetailGen
 
 urlpatterns = [
-    path("buses/", bus_list, name="bus-list")
+    path("buses/", BusListGen.as_view(), name="bus-list"),
+    path("buses/<int:pk>/", BusDetailGen.as_view(), name="bus-detail"),
 ]
 
 app_name = "station"
--- station/views.py
+++ station/views.py
@@ -1,6 +1,9 @@
-from rest_framework import status
+from django.http import Http404
+from rest_framework import status, generics, mixins
 from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from station.models import Bus
 from station.serializers import BusSerializer
@@ -21,3 +24,102 @@ def bus_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BusList(APIView):
+    def get(self, request):
+        buses = Bus.objects.all()
+        serializer = BusSerializer(buses, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = BusSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BusListGen(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 generics.GenericAPIView):
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+@api_view(["PUT", "GET", "DELETE"])
+def bus_detail(request, pk):
+    try:
+        bus = Bus.objects.get(pk=pk)
+    except Bus.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = BusSerializer(bus)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = BusSerializer(bus, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        bus.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BusDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Bus.objects.get(pk=pk)
+        except Bus.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        bus = self.get_object(pk)
+        serializer = BusSerializer(bus)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        bus = self.get_object(pk)
+        data = JSONParser().parse(request)
+        serializer = BusSerializer(bus, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        bus = self.get_object(pk)
+        bus.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BusDetailGen(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView):
+
+    queryset = Bus.objects.all()
+    serializer_class = BusSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
